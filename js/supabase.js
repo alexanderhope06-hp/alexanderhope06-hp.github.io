573c8940312128aebab88666b415bc2ca7cdf116
alexanderhope06-hp <alexanderhope06@gmail.com>
2026-09-22 11:46:31 +0200

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,18 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://pgnkpwhnrmjciqnfujjn.supabase.co';
+
+/* =====================================================
+   STORYNEST - SUPABASE CONNECTION
+   ===================================================== */
+
+const SUPABASE_URL =
+    "https://cddiifvhdnaeabfdowhj.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    'sb_publishable_F22XDwRFlSoUo_VsODx9gQ_XuJrSkQk';
+    "sb_publishable_JEL9oQPTOWwFtxvM7AdnXw_13zaBggG";
 
-export const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
